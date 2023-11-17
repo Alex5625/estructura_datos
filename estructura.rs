dff5725a08fa272aca7282aefd18c28794d24aeb
Alex5625 <ahernandez23@alumnos.utalca.cl>
2023-11-17 17:10:46 -0300

    act. modelo opcional para recorrer base de datos

--- estructura.rs
+++ estructura.rs
@@ -4,7 +4,7 @@ use std::path::Path;
 use std::u32;
 use std::fs::File;
 
-
+//lee la linea y entrega la linea completa, sin sus separaciones. Esta dentro de las clases y las propuestas solucion de guia anterior.
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
     let file = File::open(filename)?;
@@ -12,22 +12,25 @@ where P: AsRef<Path>, {
 }
 
 fn main() {
+    //cuenta en la linea que va, como control de donde va 
     let mut contador_lineas = 0;
     if let Ok(lines) = read_lines("./NOMBRE_DEL_ARCHIVO (CON SU EXTENSION .TXT O .CSV)") {
 
         for line in lines {
 
             if let Ok(ip) = line {
+
                 let ip_copy = ip.clone();
                 //                 aqui abajo modificar el tipo de split
                 let split = ip_copy.split("::");
-                //cuenta la columna, como si fuera un arreglo
+ 
+               //cuenta la columna, como si fuera un arreglo
                 let mut contador_columnas:u32 = 0;
+
                 for s in split {
                     //CUERPO DEL EJERCICIO
                 }   
             }
-            //cuenta en la linea que va, como control de donde va 
             contador_lineas += 1;
         }
     }
