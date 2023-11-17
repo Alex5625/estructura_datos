45f5415bde9c08f0ccbe791505ef8f167434ab74
Alex5625 <ahernandez23@alumnos.utalca.cl>
2023-11-17 17:01:03 -0300

    modelo opcional para recorrer base de datos

---
+++ estructura.rs
@@ -0,0 +1,33 @@
+use std::io::BufReader;
+use std::io::{self, BufRead};
+use std::path::Path;
+use std::u32;
+use std::fs::File;
+
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(BufReader::new(file).lines())
+}
+
+fn main() {
+    let mut contador_lineas = 0;
+    if let Ok(lines) = read_lines("./NOMBRE_DEL_ARCHIVO (CON SU EXTENSION .TXT O .CSV)") {
+
+        for line in lines {
+
+            if let Ok(ip) = line {
+                let ip_copy = ip.clone();
+                let split = ip_copy.split(",");
+                //cuenta la columna, como si fuera un arreglo
+                let mut contador_columnas:u32 = 0;
+                for s in split {
+                    //CUERPO DEL EJERCICIO
+                }   
+            }
+            //cuenta en la linea que va, como control de donde va 
+            contador_lineas += 1;
+        }
+    }
+}
